c4a599cc34136177200f5fa7968ac5d4d96ad161
loubruness <94389521+loubruness@users.noreply.github.com>
2024-06-19 08:09:11 +0200

    Update model_reduced.py

--- model_reduced.py
+++ model_reduced.py
@@ -4,20 +4,21 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, roc_auc_score, log_loss
 
-# Step 1: Read the CSV file
+
 file_path = 'c:/Users/pauli/Documents/M1/S8/ML/Projet/divorce.csv'
 data = pd.read_csv(file_path)
 
 # Clean the dataset
 data = data.dropna()
 
-# Select relevant columns for regression (assuming 'X' is the feature and 'Y' is the target)
+# Select relevant columns for regression
 X = data.drop(['Divorce'], axis=1)
 Y = data['Divorce']
 
 # Split the dataset into training and testing sets
 X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.3)
 
+# Selecting only the most significant features
 X_train = X_train[['Q1', 'Q3',  'Q6', 'Q8', 'Q9', 'Q11', 'Q14', 'Q15', 'Q17', 'Q18', 'Q20', 'Q25', 'Q26','Q30', 'Q31', 'Q32', 'Q33', 'Q34', 'Q36', 'Q37', 'Q38', 'Q39', 'Q40', 'Q41', 'Q44', 'Q49', 'Q50', 'Q52', 'Q53', 'Q54']]
 X_test = X_test[['Q1', 'Q3',  'Q6', 'Q8', 'Q9', 'Q11', 'Q14', 'Q15', 'Q17', 'Q18', 'Q20', 'Q25', 'Q26','Q30', 'Q31', 'Q32', 'Q33', 'Q34', 'Q36', 'Q37', 'Q38', 'Q39', 'Q40', 'Q41', 'Q44', 'Q49', 'Q50', 'Q52', 'Q53', 'Q54']]
 
